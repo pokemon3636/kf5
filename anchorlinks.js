8b3ecfe39ccbd819e2a0997c020cc3a5bac009bd
slgu <slgu@cybozu.net.cn>
2019-08-21 16:19:23 +0800

    1

---
+++ anchorlinks.js
@@ -0,0 +1,32 @@
+(function ($) {
+    $(function () {
+        var $hrefList = $(".anchor-link li a");
+        if ($hrefList.length > 0) {
+            var anchorLinks = setAnchorLink();
+            setLink(anchorLinks);
+        }
+    });
+    function setAnchorLink() {
+        var anchorLinks = [];
+        var $hrefList = $(".anchor-link li a");
+        $hrefList.each(function (index, element) {
+            var href = $(element).attr("href");
+            var indexOfAnchorLink = href.lastIndexOf("\#");
+            if (indexOfAnchorLink >= 0) {
+                var anchorLink = href.substring(indexOfAnchorLink + 1, href.length);
+                anchorLinks[$(element).text()] = anchorLink;
+            }
+        });
+        return anchorLinks;
+    }
+
+    function setLink(anchorLinks) {
+        var $headers = $(":header");
+        $headers.each(function (index, element) {
+            var headerName = $(element).text().replace(/(^\s*)|(\s*$)/g, "");
+            if (anchorLinks[headerName]) {
+                $(element).attr('id', anchorLinks[headerName]);
+            }
+        });
+    }
+})(jQuery);
---
+++ kf5.js
@@ -0,0 +1,21 @@
+//因为为了获取user_id所以用windows.load
+window.onload = function () {
+	if ($('.iframeFormbridge').length === 1) {
+		if ($('.user-info-name [href]').length > 0) {
+			var url = encodeURIComponent(window.location.href);
+			var hostname = window.location.host;
+			window.location.href = "http://" + hostname + "/user/login/?request=" + url;
+		}
+		else {
+			var $iframe = $('.iframeFormbridge');
+			$iframe[0].contentWindow.postMessage(user_id, '*');
+		}
+	}
+}
+
+
+$(function () {
+	if ($('.iframeFormbridge').length === 1) {
+		$("#article-comments, .share-list, .next_prev_wrap, .next_prev_wrap").hide();
+	}
+});
